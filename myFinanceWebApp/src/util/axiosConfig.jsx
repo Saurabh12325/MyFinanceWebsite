66ab113c5faae7b3b5428b320763943dba7777f4
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-08-28 17:01:34 +0530

    changes

--- myFinanceWebApp/src/util/axiosConfig.jsx
+++ myFinanceWebApp/src/util/axiosConfig.jsx
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const axiosConfig = axios.create({
     baseURL: 'https://myfinance-p2v8.onrender.com', // Replace with your API base URL \
-    headers:{
+    headers: {
         "Content-Type": "application/json",
-        Accept : "application/json"
+        Accept: "application/json"
 
     }
 });
@@ -13,17 +13,40 @@ const axiosConfig = axios.create({
 const excludeEndPoints = ["/login", "/register", "/activate", "/health"];
 
 //request Interceptor
-axiosConfig.interceptors.request.use((config => {
-    const shouldSkipToken = excludeEndPoints.some((endPoint) => 
-        config.url?.includes(endPoint))
+axiosConfig.interceptors.request.use(
+    (config) => {
+        const shouldSkipToken = excludeEndPoints.some((endPoint) =>
+            config.url?.includes(endPoint)
+        );
 
-    if(!shouldSkipToken) {
-   const accessToken =  localStorage.getItem("token");
-   if(accessToken) {
-    config.headers["Authorization"] = `Bearer ${accessToken}`;
-   }
+        if (!shouldSkipToken) {
+            const accessToken = localStorage.getItem("token");
+            if (accessToken) {
+                config.headers["Authorization"] = `Bearer ${accessToken}`;
+            }
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+
+//response interceptor
+axiosConfig.interceptors.response.use((response) => {
+    return response;
+}, (error) => {
+    if(error.response){
+         if(error.response.status === 401){
+            window.location.href = "/login";
+         }
+         else if(error.response.status === 500){
+             console.error("Server error . Please Try again later");
+         }
+         else if (error.code === "ECONNABORTED"){
+            console.error("Request TimeOut, Please Try again")
+
+         }
+         return Promise.reject(error);
     }
-    return config
-}
-))
-
+    
+})
