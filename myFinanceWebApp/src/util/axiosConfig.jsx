2bf4bbf5fbb3ea2e104255a2f98819ded8e35b43
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-08-28 16:46:25 +0530

    changes

--- myFinanceWebApp/src/util/axiosConfig.jsx
+++ myFinanceWebApp/src/util/axiosConfig.jsx
@@ -1,5 +1,29 @@
 import axios from 'axios';
 
 const axiosConfig = axios.create({
-    baseURL: 'https://myfinance-p2v8.onrender.com', // Replace with your API base URL 
-})
+    baseURL: 'https://myfinance-p2v8.onrender.com', // Replace with your API base URL \
+    headers:{
+        "Content-Type": "application/json",
+        Accept : "application/json"
+
+    }
+});
+//list of the endpoints
+
+const excludeEndPoints = ["/login", "/register", "/activate", "/health"];
+
+//request Interceptor
+axiosConfig.interceptors.request.use((config => {
+    const shouldSkipToken = excludeEndPoints.some((endPoint) => 
+        config.url?.includes(endPoint))
+
+    if(!shouldSkipToken) {
+   const accessToken =  localStorage.getItem("token");
+   if(accessToken) {
+    config.headers["Authorization"] = `Bearer ${accessToken}`;
+   }
+    }
+    return config
+}
+))
+
