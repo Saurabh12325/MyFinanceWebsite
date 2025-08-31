5e90c0f1da51d0724d213f3d299d86ce4a13aadd
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-09-01 00:20:32 +0530

    changes

--- myFinanceWebApp/src/pages/Input.jsx
+++ myFinanceWebApp/src/pages/Input.jsx
@@ -1,6 +1,44 @@
-const Input = () => {
+import React, { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
+const Input = ({label,value,onChange,placeholder,type}) => {
+    const[showPassword,setShowPassword] = useState(false)
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    }
     return (
-        <div>Input</div>
+        <div className="mb-4" >
+            <label className="text text-slate-800 block mb-1">{label}</label>
+            <div className="relative">
+                 <input 
+                 className="w-full bg-transparent outline-none border  border-gray-300 rounded-md py-2 px-3 pr-10 text-gray-900 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 leading-tight focus:outline-none"
+                 type={type === "password"? (showPassword ? "text" : "password") : type} 
+                 value={value} 
+                 onChange={(e)=>onChange(e)} 
+                 placeholder={placeholder}
+                 />
+                 {type === "password" && (
+                    <span className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer text-gray-500">
+                        {showPassword ?( 
+                            <Eye
+                            size={20}
+                            onClick={togglePasswordVisibility}
+                            className="text-primary text-red-500"
+                            />
+                        ):(
+                            <EyeOff
+                            size={20}   
+                            onClick={togglePasswordVisibility}
+                            className="text-primary"
+                            />
+                        )}
+                    </span>
+                    
+                 )}
+                 
+            </div>
+        </div>
+        
     )
     }
     export default Input
--- myFinanceWebApp/src/pages/SignUp.jsx
+++ myFinanceWebApp/src/pages/SignUp.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { assets } from "../assets/asset.js";
+import Input from "./Input.jsx";
+import { Eye, EyeOff } from "lucide-react";
 
 function SignUp() {
   const [fullName, setFullName] = useState("");
@@ -27,8 +29,39 @@ function SignUp() {
 
                   </div>
                  <div className="grid grid-cols-2 md:grid-cols-2 gap-4">
-                      
+                      <Input
+                         value={fullName}
+                         onChange={(e)=>setFullName(e.target.value)}
+                         label="Full Name"
+                          placeholder="John Doe"
+                          type="text"
+                      />
+                      <Input
+                         value={email}
+                         onChange={(e)=>setEmail(e.target.value)}
+                         label="Email"
+                          placeholder="name@example.com"
+                          type="text"
+                      />
+                    <div className="col-span-2">
+                        <Input
+                         value={password}
+                         onChange={(e)=>SetPassword(e.target.value)}
+                         label="Password"
+                          placeholder="************"
+                          type="text"
+                      />
+                    </div>
+
                  </div>
+                 {
+                  error && <p className="text-red-800 text-sm text-center bg-red-50">{error}</p>
+                 }
+                 <button className="bg-slate-900 rounded-2xl w-full py-3 text-lg text-white font-medium" type="submit">
+                    Sign Up
+                 </button>
+                  <p className="text-sm  text-slate-800 text-center mt-6">Already have an account? 
+                    <span onClick={()=>navigate('/login')} className="font-medium text-primary underline hover:text-primary-dark transition-colors cursor-pointer">Login</span> </p>
                   </form>
          </div>
       </div>
