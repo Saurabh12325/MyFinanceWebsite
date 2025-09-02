96821bd25985e87381c86c5a0d7045592255cada
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-09-03 00:13:46 +0530

    changes

--- myFinanceWebApp/src/pages/Login.jsx
+++ myFinanceWebApp/src/pages/Login.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Input from './Input';
 
+ const navigate = useNavigate();
 function Login() {
 
       const [fullName, setFullName] = useState("");
