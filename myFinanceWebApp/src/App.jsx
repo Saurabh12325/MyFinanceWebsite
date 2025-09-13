733839e5201f748d1f4807772d6f484a66762fc6
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-09-13 12:54:19 +0530

    changes

--- myFinanceWebApp/src/App.jsx
+++ myFinanceWebApp/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route ,Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Inome from './pages/Income.jsx'
 import Expense from './pages/Expense.jsx'
@@ -8,22 +8,26 @@ import Login from './pages/Login.jsx'
 import Category from './pages/Category.jsx'
 import SignUp from './pages/SignUp.jsx'
 import { Toaster } from "react-hot-toast";
+import Nav from './pages/Nav.jsx'
 
 function App() {
   return (
     <>
-    <Toaster/>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/dashboard" element={<Home/>} />
-      <Route path="/income" element={<Inome/>} />
-      <Route path="/expense" element={<Expense/>} />
-      <Route path="/filter" element={<Filter/>} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="/signUp" element={<SignUp/>} />
-      <Route path="/category" element={<Category/>} /> 
-    </Routes>
-    </BrowserRouter>
+      <Toaster />
+      <BrowserRouter>
+        <Nav /> 
+        <div className="p-1">
+          <Routes>
+            <Route path="/dashboard" element={<Home />} />
+            <Route path="/income" element={<Inome />} />
+            <Route path="/expense" element={<Expense />} />
+            <Route path="/filter" element={<Filter />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signUp" element={<SignUp />} />
+            <Route path="/category" element={<Category />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   )
 }
---
+++ myFinanceWebApp/src/pages/Nav.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white p-4 shadow-md">
+      <ul className="flex space-x-6">
+        <li>
+          <Link to="/dashboard" className="hover:text-yellow-300">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/income" className="hover:text-yellow-300">Income</Link>
+        </li>
+        <li>
+          <Link to="/expense" className="hover:text-yellow-300">Expense</Link>
+        </li>
+        <li>
+          <Link to="/filter" className="hover:text-yellow-300">Filter</Link>
+        </li>
+        <li>
+          <Link to="/category" className="hover:text-yellow-300">Category</Link>
+        </li>
+        <li>
+          <Link to="/login" className="hover:text-yellow-300">Login</Link>
+        </li>
+        <li>
+          <Link to="/signUp" className="hover:text-yellow-300">Sign Up</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
