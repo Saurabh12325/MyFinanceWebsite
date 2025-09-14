9a4b62e00181cdec5ba0480659b45aaa3fae4c15
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-09-14 17:04:24 +0530

    cloudinarySetUp

--- myFinanceWebApp/src/util/uploadProfileImage.js
+++ myFinanceWebApp/src/util/uploadProfileImage.js
@@ -1,1 +1,7 @@
 const CLOUDINARY_UPLOAD_PRESET = "MyFinance";
+
+export const uploadProfileImage = async (imageFile) => {
+    const formData = new FormData();
+    formData.append("file", imageFile);
+    formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
+}
