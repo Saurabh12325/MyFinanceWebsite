994e3d623166a2b7ed8079f29612a3b3af8a5ff8
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-09-16 17:14:53 +0530

    changes

--- myFinanceWebApp/src/pages/Landing.jsx
+++ myFinanceWebApp/src/pages/Landing.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from "react";
+import { ReactTyped } from "react-typed";
 
 function Landing() {
-    return (
-        <>
-        <div className='w-full h-screen flex justify-center items-center bg-gradient-to-b from-black via-black to-emerald-400'>
-            <div>
-                <h1>Welcome to the My Finance </h1>
-            </div>
+  return (
+    <>
+      <div className="w-full h-screen flex justify-center items-center bg-gradient-to-b from-black via-black to-emerald-400">
+        <div className="text-white text-center text-4xl font-bold w-3/4 relative bottom-5">
+          <ReactTyped
+            strings={[
+              "Welcome to MyFinance – your smart platform for financial growth and management.",
+              "Track your expenses easily 💰",
+              "Manage your budget smartly 📊",
+              "Grow your wealth with confidence 🚀"
+            ]}
+            typeSpeed={50}
+            backSpeed={30}
+            loop
+          />
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
-export default Landing
+export default Landing;
---
+++ myFinanceWebApp/src/pages/SplashScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SplashScreen() {
+    return (
+        
+    )
+}
+
+export default SplashScreen
