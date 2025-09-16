517c5d380fa432441324618a3b175d3d33024378
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-09-17 00:47:40 +0530

    changes

--- myFinanceWebApp/src/pages/SplashScreen.jsx
+++ myFinanceWebApp/src/pages/SplashScreen.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import { assets } from '../assets/asset'
+import {motion} from 'framer-motion'
 function SplashScreen() {
     return (
-        <div className='min-h-screen w-full'>fbjrjblrejller</div>
+        <div className='min-h-screen w-full  bg-gradient-to-t from-emerald-400 via-black to-black flex justify-center items-center '>
+            <motion.img 
+             className=' object-contain '
+             src={assets.Splash} 
+             alt="Splash"
+             initial={{ scale: 0 }}
+             animate={{ rotate: 360, scale: 1.5 }}
+             transition={{duration: 4, ease: 'easeInOut'}}
+
+             />
+        
+           
+        </div>
 
     )
 }
