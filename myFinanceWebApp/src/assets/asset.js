cd25f68d0f30541b270f91d7a7cca604efa9dbad
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-09-17 10:41:45 +0530

    changes

--- myFinanceWebApp/src/assets/asset.js
+++ myFinanceWebApp/src/assets/asset.js
@@ -2,15 +2,13 @@ import logo from './logo.png';
 import bg from './bg.jpg';
 import register from './register.avif';
 import login from './login.jpg';
-import splash from './splash.png';
- 
 
 export const assets = {
     logo,
     bg,
     register,
     login,
-    splash
-
+    
+    
 
 };
--- myFinanceWebApp/src/pages/SplashScreen.jsx
+++ myFinanceWebApp/src/pages/SplashScreen.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { assets } from '../assets/asset'
+import { assets } from '../assets/asset.js'
 import {motion} from 'framer-motion'
 function SplashScreen() {
     return (
         <div className='min-h-screen w-full  bg-gradient-to-t from-emerald-400 via-black to-black flex justify-center items-center '>
             <motion.img 
              className=' object-contain '
-             src={assets.splash} 
+             src={assets.logo}
              alt="Splash"
              initial={{ scale: 0 }}
              animate={{ rotate: 360, scale: 1.5 }}
