0db76a6623ead8f4d23679bbef3e59f26efa102e
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-09-22 10:33:58 +0530

    changes

--- myFinanceWebApp/src/components/MenuBar.jsx
+++ myFinanceWebApp/src/components/MenuBar.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { AppContext } from '../Context/AppContext';
+import { Menu, X } from 'lucide-react';
 
 function MenuBar() {
     const[openSidebar,setOpenSidebar]=useState(false)
@@ -12,7 +13,13 @@ function MenuBar() {
     return (
         <div className='flex itam-center justify-between gap-5 bg-white border border-b border-graey-200/5'>
             {/* Left Section -Menu button and tittle  */}
-            <span> Left side -Menu button and tittle</span>
+            <div className="flex item-center gap-5">
+                <button 
+                onClick={()=>setOpenSidebar(!openSidebar)}
+                className='block lg:hidden text-2xl p-2 hover:bg-gray-100  rounded-md'>
+                    {openSidebar ? <X className='text-black text-2xl'/> : <Menu className='text-black text-2xl'/>}
+                </button>
+            </div>
             {/*Right side -Avatar photo*/}
             <span> right side -Menu button and tittle</span>
             {/*Mobile side Menu*/}
