7baa55d39844a4dcf792d9ce3447ee1a7c497b78
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-09-24 21:53:49 +0530

    changes

--- myFinanceWebApp/src/pages/Landing.jsx
+++ myFinanceWebApp/src/pages/Landing.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { ReactTyped } from "react-typed";
 
 function Landing() {
+  const navigate = useNavigate();
+  const handleNavigate =()=>{
+    navigate('/signup')
+  }
   return (
     <>
       <div className="w-full h-screen flex justify-center items-center bg-gradient-to-b from-black via-black to-emerald-500">
@@ -20,7 +25,9 @@ function Landing() {
           
         </div>
         <div className="absolute  backdrop-blur-sm bg-white/0 p-4 rounded-2xl border-2 hover:scale-105 duration-300  text-white text-2xl font-semibold cursor-pointer hover:bg-emerald-500 animate-bounce">
-          <button className="cursor-pointer shadow-2xl hover:text-black" >SignUp 
+          <button
+          onClick={handleNavigate}
+           className="cursor-pointer shadow-2xl hover:text-black" >SignUp 
             
           </button>
         </div>
