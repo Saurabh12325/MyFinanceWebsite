292775346c43785209e3831ff2dce75b15678f8b
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-09-25 21:11:42 +0530

    changes

--- myFinanceWebApp/src/components/Dashboard.jsx
+++ myFinanceWebApp/src/components/Dashboard.jsx
@@ -1,9 +1,13 @@
+import { useContext } from "react"
 import MenuBar from "./MenuBar"
+import { AppContext } from "../Context/AppContext"
 
 const Dashboard = () => {
+    const {user} = useContext(AppContext)
     return (    
         <div>
             <MenuBar/>
+
         </div>
     )
 }
