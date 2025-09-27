ba8e1d5556b2fe365df4ac16a2044a00801ea081
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-09-28 00:54:50 +0530

    responsive

--- myFinanceWebApp/src/pages/Landing.jsx
+++ myFinanceWebApp/src/pages/Landing.jsx
@@ -13,7 +13,7 @@ function Landing() {
   return (
     <>
       <div className="w-full h-screen flex justify-center items-center bg-gradient-to-b from-black via-black to-emerald-500">
-        <div className="text-white text-center text-5xl font-bold font-serif w-3/5 relative bottom-40">
+        <div className="text-white text-center text-5xl font-bold font-serif w-3/ relative bottom-40 max-md:text-3xl  ">
           <ReactTyped
             strings={[
               "Welcome to MyFinance – your smart platform for financial growth and management.",
@@ -27,7 +27,7 @@ function Landing() {
           />
           
         </div>
-          <div className="absolute p-4 flex justify-center items-center ">
+          <div className="absolute p-4 flex justify-center items-center  ">
         <div className=" backdrop-blur-sm mx-5 bg-white/0 p-4 rounded-2xl border-2 hover:scale-105 duration-300  text-white text-2xl font-semibold cursor-pointer hover:bg-emerald-500 animate-bounce">
           <button
           onClick={handleNavigate}
