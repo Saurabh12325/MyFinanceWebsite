b609f408820b65cd588a3cfa5965c227dd30bb53
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-10-03 20:23:18 +0530

    changes

--- myFinanceWebApp/src/util/apiEndPoint.js
+++ myFinanceWebApp/src/util/apiEndPoint.js
@@ -1,8 +1,10 @@
-export const BASE_URL = "https://myfinance-p2v8.onrender.com/api/v1.0";
+// export const BASE_URL = "https://myfinance-p2v8.onrender.com/api/v1.0";
+export const BASE_URL = "http://localhost:8080/api/v1.0";
 const CLOUDINARY_CLOUD_NAME = "dtmnghjah";
 export const API_ENDPOINTS = {
    LOGIN : "/profile/login",
    REGISTER : "/profile/register",
+   PROFILE:"/profile/profileinfo",
    UPLOAD_IMAGE : `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`,
    
 
