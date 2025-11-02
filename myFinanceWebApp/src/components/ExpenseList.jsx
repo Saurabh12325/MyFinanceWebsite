ff9c525534946c180ef16698dc8d7612c850c9be
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-11-02 17:02:23 +0530

    newchange

--- myFinanceWebApp/src/components/ExpenseList.jsx
+++ myFinanceWebApp/src/components/ExpenseList.jsx
@@ -43,14 +43,14 @@ const ExpenseList = ({ expenses = [], onDelete }) => {
   };
 
   return (
-    <div className="card p-4 shadow-xl">
+    <div className="card p-4 shadow-xl border mt-2.5 rounded-md ">
       <div className="flex items-center justify-between mb-4">
-        <h4 className="text-lg font-semibold">Expense Records</h4>
-        <div className="flex items-center gap-2">
-          <button onClick={sendEmail} className="card-btn flex items-center gap-1">
+        <h4 className="text-lg font-bold">Expense Records</h4>
+        <div className="flex items-center gap-2 ">
+          <button onClick={sendEmail} className="flex items-center font-bold gap-2 text-black-900 cursor-pointer bg-emerald-50 px-4 py-2 mt-1 border rounded-lg hover:bg-emerald-100 transition">
             <Mail size={15} /> Email
           </button>
-          <button onClick={downloadExcel} className="card-btn flex items-center gap-1">
+          <button onClick={downloadExcel} className="flex items-center font-bold gap-2 text-black-900 cursor-pointer bg-emerald-50 px-4 py-2 mt-1 border rounded-lg hover:bg-emerald-100 transition">
             <Download size={15} /> Download
           </button>
         </div>
@@ -59,14 +59,14 @@ const ExpenseList = ({ expenses = [], onDelete }) => {
       {expenses.length === 0 ? (
         <p className="text-gray-500">No expense records added yet. Add some to get started!</p>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
           {expenses.map((expense) => (
             <div
               key={expense._id || expense.id}
-              className="border p-4 rounded-lg shadow-sm flex justify-between items-center cursor-pointer hover:bg-gray-100 transition-all duration-200"
+              className="border p-4 rounded-lg shadow-sm flex justify-between items-center  bg-white"
             >
               <div className="w-12 h-12 flex items-center justify-center text-xl text-gray-800 bg-gray-100 rounded-full">
-                <Layers2 size={24} className="text-red-700" />
+                <Layers2 size={24} className="text-red-700 " />
               </div>
               <div className="flex-1 ml-4">
                 <h5 className="font-semibold text-gray-900">{expense.name}</h5>
@@ -78,7 +78,7 @@ const ExpenseList = ({ expenses = [], onDelete }) => {
                   onClick={() => onDelete(expense.id || expense._id)}
                   className="text-red-500 hover:text-red-700"
                 >
-                  <Trash2 size={18} />
+                  <Trash2 size={18} className="cursor-pointer" />
                 </button>
               </div>
             </div>
